6a7ba0bcafe24e7dcd1fd41ad90e682146af87fc
Kat Tochowicz <ktochow1@binghamton.edu>
2020-04-13 17:34:24 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+let divide = () => {
+  let answer = 2000 / 100;
+  return answer
+}
+
+let square = (num) => {
+  let answer = num * num
+  return answer
+}
+
+let add = (num1, num2) => {
+  let answer = num1 + num2;
+  return answer
+}
