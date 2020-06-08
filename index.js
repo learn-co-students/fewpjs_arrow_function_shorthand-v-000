294f23b03662756e1476b3d664f49e911b61182b
github username <github email address>
2020-06-07 22:27:31 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+// let divide = (num1, num2) => num2 / num1
+// // console.log(num1)
+// console.log(divide(2000, 100))
+//
+
+// let divide = function(num1, num2) {
+//   return (num1 / num2)
+// }
+
+let divide = function() {
+  return (2000 / 100)
+}
+// return divide(2000, 10)
+
+
+let square = (num) => num ** num
+
+
+let add = (num1, num2) => num1 + num2
