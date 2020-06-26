4867fd57f082863f85e70511e1700b6767d14666
benitolsantos <github email address>
2020-06-26 18:13:31 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function divide() {
+    return 2000/100
+}
+
+/* Arrow Function
+
+let add = (parameter1, parameter2) => parameter1 + parameter2
+add(2,3) //=> 5
+
+*/
+
+let square = (number) => {
+ return number * number
+}
+
+let add = (number1,number2) => {
+    return number1 + number2
+}
